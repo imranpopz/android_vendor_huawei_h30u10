00d3d579a97601d5a1994acc504ded6b9c337ae2
imranpopz <imranpopz32@gmail.com>
2020-05-31 22:45:42 +0530

    Build libs from source

--- Android.mk
+++ Android.mk
@@ -14,56 +14,68 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter h30u10, $(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := mtk-ril
-LOCAL_MODULE_OWNER :=google
-LOCAL_SRC_FILES := proprietary/lib/mtk-ril.so
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE := libion_mtk
+LOCAL_SRC_FILES := proprietary/lib/libion_mtk.so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam1client
+LOCAL_SRC_FILES := proprietary/lib/libcam1client.so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcam_utils
+LOCAL_SRC_FILES := proprietary/lib/libcam_utils.so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libged
+LOCAL_SRC_FILES := proprietary/lib/libged.so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libbwc
+LOCAL_SRC_FILES := proprietary/lib/libbwc.so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libperfservicenative
+LOCAL_SRC_FILES := proprietary/lib/libperfservicenative.so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := librilmtk
-LOCAL_MODULE_OWNER := google
 LOCAL_SRC_FILES := proprietary/lib/librilmtk.so
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
-
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := libui_ext
-#LOCAL_SRC_FILES := proprietary/lib/libui_ext.so
-#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-#LOCAL_MODULE_SUFFIX := .so
-#include $(BUILD_PREBUILT)
-
-
-ifeq ($(USE_MTK_CAMERA_WRAPPER),true) 
-
-#include $(CLEAR_VARS) 
-#LOCAL_MODULE := libcam1client 
-#LOCAL_SRC_FILES := proprietary/lib/libcam1client.so 
-#LOCAL_MODULE_CLASS := SHARED_LIBRARIES 
-#LOCAL_MODULE_SUFFIX := .so 
-#include $(BUILD_PREBUILT) 
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libcam_utils
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/lib/libcam_utils.so
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := mtk-ril
+LOCAL_SRC_FILES := proprietary/lib/mtk-ril.so
+LOCAL_PROPRIETARY_MODULE = true
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
-endif
 
-
-endif
